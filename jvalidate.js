d86d986e273869a383034b12ed1f6afb83dc4772
ArmelNL <armel@armel.nl>
2012-10-16 18:59:24 +0200

    Revert "merged validate function"
    This reverts commit 039695d5b5f85586a8732e493068120752027ac4, reversing
    changes made to e4e83368596879a9c33a8551fd3f790f1f53cb0b.

--- jvalidate.js
+++ jvalidate.js
@@ -34,10 +34,8 @@ by Armel van Ravels and Dominique de Brabander
 			'successCallback' : $.noop
 		},
 		url : {
-
-			'validateFunction' : function(value){
-				return /_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS
-/.test(value); // by @diegoperini
+			'validateFunction' : function( value ){
+				return /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(value);
 			},
 			'errorCallback' : $.noop,
 			'successCallback' : $.noop
